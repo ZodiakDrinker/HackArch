7107002bdf0e587164ab0bd5cf58f5fb7bc533de
ZodiakDrinker <mateoaja11@gmail.com>
2026-09-03 21:18:32 +0200

    adding profiledef.sh

---
+++ profile_archiso/profiledef.sh
@@ -0,0 +1,20 @@
+iso_name="hackarch"
+iso_label="HACKARCH_LIVE"
+iso_publisher="ZodiakDrinker"
+iso_application="HackArch Live Pentesting OS"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x86_64.grub.esp')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+hostname="hackarch"
+
+file_permissions=(
+  ["/etc/profile.d/create-user.sh"]="0:0:755"
+  ["/usr/local/bin/*"]="0:0:755"
+  ["/etc/skel/.ssh"]="0:0:700"
+  ["/etc/skel/.ssh/id_ed25519"]="0:0:600"
+  ["/etc/skel/.ssh/id_ed25519.pub"]="0:0:644"
+)
