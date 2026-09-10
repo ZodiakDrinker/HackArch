bc51310822ff88a8c8b393998d4409b58cbc3e6a
ZodiakDrinker <mateoaja11@gmail.com>
2026-09-10 20:19:02 +0200

    successful boot

--- profile_archiso/airootfs/root/customize_airootfs.sh
+++ profile_archiso/airootfs/root/customize_airootfs.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-if ! id zodiak >/dev/null 2>&1; then
+USUARIO="zodiak"
+if ! id $USUARIO >/dev/null 2>&1; then
     useradd \
         -m \
         -k /etc/skel \
         -u 1000 \
         -G wheel,video,audio,storage,network \
         -s /bin/zsh \
-        zodiak
+        $USUARIO
 fi
 
-passwd -d zodiak
+passwd -d $USUARIO
 
 install -d -m 0750 /etc/sudoers.d
-printf '%s\n' 'zodiak ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/zodiak
-chmod 440 /etc/sudoers.d/zodiak
+printf '%s\n' "$USUARIO ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USUARIO
+chmod 440 /etc/sudoers.d/$USUARIO
+chmod +x /home/$USUARIO/.config/bspwm/bspwmrc
+chmod +x /home/$USUARIO/.config/polybar/ -R
